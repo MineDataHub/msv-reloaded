209df2491b44aedc79c0877191a3b600dc692ede
NecRoZ2 <165396259+NecRoZ2@users.noreply.github.com>
2024-08-05 20:00:52 +0500

    убрал лишнее

--- src/main/java/datahub/msv/MSVNbtTags.kt
+++ src/main/java/datahub/msv/MSVNbtTags.kt
@@ -1,10 +1,6 @@
 package datahub.msv
 
-import net.minecraft.nbt.NbtCompound
-import net.minecraft.server.network.ServerPlayerEntity
-
 object MSVNbtTags{
-    val nbt = NbtCompound()
     public final val MSVTAGS = "msv_tags"
     public final val FREEZE_COOLDOWN = "freeze_cooldown"
     public final val SNEEZE_COOLDOWN = "sneeze_cooldown"
@@ -13,7 +9,4 @@ object MSVNbtTags{
     public final val SLOWNESS_COOLDOWN = "slowness_cooldown"
     public final val MINING_FATIGUE_COOLDOWN = "mining_fatigue_cooldown"
     public final val CREEPER_SOUND_COOLDOWN = "creeper_sound_cooldown"
-    fun freeze_cooldown(player: ServerPlayerEntity) {
-        player.writeCustomDataToNbt(nbt)
-    }
 }
