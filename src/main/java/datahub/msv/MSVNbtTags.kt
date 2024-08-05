f081b29e4fd23db4964a58cd4edbf3e656f0e046
NecRoZ2 <165396259+NecRoZ2@users.noreply.github.com>
2024-08-05 19:59:20 +0500

    обавил новые теги для игрока, но пока не записал их в 1 тег

--- src/main/java/datahub/msv/MSVNbtTags.kt
+++ src/main/java/datahub/msv/MSVNbtTags.kt
@@ -1,5 +1,19 @@
 package datahub.msv
 
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.server.network.ServerPlayerEntity
+
 object MSVNbtTags{
+    val nbt = NbtCompound()
+    public final val MSVTAGS = "msv_tags"
     public final val FREEZE_COOLDOWN = "freeze_cooldown"
+    public final val SNEEZE_COOLDOWN = "sneeze_cooldown"
+    public final val BLINDNESS_COOLDOWN = "blindness_cooldown"
+    public final val NAUSEA_COOLDOWN = "nausea_cooldown"
+    public final val SLOWNESS_COOLDOWN = "slowness_cooldown"
+    public final val MINING_FATIGUE_COOLDOWN = "mining_fatigue_cooldown"
+    public final val CREEPER_SOUND_COOLDOWN = "creeper_sound_cooldown"
+    fun freeze_cooldown(player: ServerPlayerEntity) {
+        player.writeCustomDataToNbt(nbt)
+    }
 }
--- src/main/java/datahub/msv/mixin/NewNbtMixin.kt
+++ src/main/java/datahub/msv/mixin/NewNbtMixin.kt
@@ -14,14 +14,34 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 abstract class NewNbtMixin {
     @Unique
     var freeze_cooldown: Int = 0
+    var sneeze_cooldown: Int = 0
+    var blindness_cooldown: Int = 0
+    var nausea_cooldown: Int = 0
+    var slowness_cooldown: Int = 0
+    var mining_fatigue_cooldown: Int = 0
+    var creeper_sound_cooldown: Int = 0
 
     @Inject(method = ["writeCustomDataToNbt"], at = [At("TAIL")])
     fun writeCustomDataToNbt(nbt: NbtCompound, ci: CallbackInfo?) {
         nbt.putInt(MSVNbtTags.FREEZE_COOLDOWN, this.freeze_cooldown)
+        nbt.putInt(MSVNbtTags.SNEEZE_COOLDOWN, this.sneeze_cooldown)
+        nbt.putInt(MSVNbtTags.BLINDNESS_COOLDOWN, this.blindness_cooldown)
+        nbt.putInt(MSVNbtTags.NAUSEA_COOLDOWN, this.nausea_cooldown)
+        nbt.putInt(MSVNbtTags.SLOWNESS_COOLDOWN, this.slowness_cooldown)
+        nbt.putInt(MSVNbtTags.MINING_FATIGUE_COOLDOWN, this.mining_fatigue_cooldown)
+        nbt.putInt(MSVNbtTags.CREEPER_SOUND_COOLDOWN, this.creeper_sound_cooldown)
     }
 
     @Inject(method = ["readCustomDataFromNbt"], at = [At("TAIL")])
     fun readCustomDataFromNbt(nbt: NbtCompound, ci: CallbackInfo?) {
         this.freeze_cooldown = nbt.getInt(MSVNbtTags.FREEZE_COOLDOWN)
+        this.sneeze_cooldown = nbt.getInt(MSVNbtTags.SNEEZE_COOLDOWN)
+        this.blindness_cooldown = nbt.getInt(MSVNbtTags.BLINDNESS_COOLDOWN)
+        this.nausea_cooldown = nbt.getInt(MSVNbtTags.NAUSEA_COOLDOWN)
+        this.slowness_cooldown = nbt.getInt(MSVNbtTags.SLOWNESS_COOLDOWN)
+        this.mining_fatigue_cooldown = nbt.getInt(MSVNbtTags.MINING_FATIGUE_COOLDOWN)
+        this.creeper_sound_cooldown = nbt.getInt(MSVNbtTags.CREEPER_SOUND_COOLDOWN)
+
+
     }
 }
