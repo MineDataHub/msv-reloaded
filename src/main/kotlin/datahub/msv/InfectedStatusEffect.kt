982c7121f27ebaed0b4559810a5c3e84750317fc
trpplvn <trpplvn@gmail.com>
2024-08-13 23:32:59 +0300

    имлементировал полимер, и написал эффект заражения

---
+++ src/main/kotlin/datahub/msv/InfectedStatusEffect.kt
@@ -0,0 +1,12 @@
+package datahub.msv
+
+import eu.pb4.polymer.core.api.other.PolymerStatusEffect
+import net.minecraft.entity.effect.StatusEffect
+import net.minecraft.entity.effect.StatusEffectCategory
+import net.minecraft.server.network.ServerPlayerEntity
+
+class InfectedStatusEffect : StatusEffect(StatusEffectCategory.HARMFUL, 0xFF0000), PolymerStatusEffect {
+    override fun getPolymerReplacement(player: ServerPlayerEntity): StatusEffect {
+        return this
+    }
+}
--- src/main/kotlin/datahub/msv/Main.kt
+++ src/main/kotlin/datahub/msv/Main.kt
@@ -1,14 +1,24 @@
 package datahub.msv
 
 import datahub.msv.Features.registerModFeatures
+import eu.pb4.polymer.core.api.other.PolymerStatusEffect
+import eu.pb4.polymer.core.api.utils.PolymerRegistry
+import eu.pb4.polymer.core.impl.PolymerMod
 import net.fabricmc.api.ModInitializer
+import net.minecraft.entity.effect.StatusEffect
+import net.minecraft.entity.effect.StatusEffectCategory
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
 import net.minecraft.util.Identifier
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+
 class Main : ModInitializer {
     override fun onInitialize() {
         registerModFeatures()
+        Registry.register(Registries.STATUS_EFFECT, id("infected"), InfectedStatusEffect())
         LOGGER.info("Hello Fabric world!")
     }
 
