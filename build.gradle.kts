b127c98171edde4af275a7b529938fd29805b1d1
trpplvn <trpplvn@gmail.com>
2024-09-07 10:42:55 +0300

    - cardinal components

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,12 +52,7 @@ dependencies {
 	modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("kotlin_loader_version")}")
 	implementation(kotlin("stdlib"))
 	modImplementation("eu.pb4:polymer-core:${project.property("polymer_version")}")
-	val ccaVersion = property("cca_version") as String
 
-	modImplementation("org.ladysnake.cardinal-components-api:cardinal-components-base:$ccaVersion")
-	modImplementation("org.ladysnake.cardinal-components-api:cardinal-components-entity:$ccaVersion")
-	include("org.ladysnake.cardinal-components-api:cardinal-components-base:$ccaVersion")
-	include("org.ladysnake.cardinal-components-api:cardinal-components-entity:$ccaVersion")
 	// Fabric API. This is technically optional, but you probably want it anyway.
 	modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
 }
