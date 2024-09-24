1a1b857957c2cad7b8c2316ef83a1429e8d62091
trpplvn <trpplvn@gmail.com>
2024-09-24 21:28:58 +0300

    улучшил поведение зараженных мобов

--- src/main/java/datahub/msv/mixin/InfectedMobsMixin.java
+++ src/main/java/datahub/msv/mixin/InfectedMobsMixin.java
@@ -15,11 +15,20 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AnimalEntity.class)
 public abstract class InfectedMobsMixin extends MobEntity {
-    protected InfectedMobsMixin(EntityType<? extends MobEntity> entityType, World world) {
+    protected InfectedMobsMixin(EntityType<? extends MobEntity> entityType, World world, PlayerEntity targetPlayer) {
         super(entityType, world);
+        this.targetPlayer = targetPlayer;
+    }
+
+    @Inject(method = "canEat", at = @At("HEAD"), cancellable = true)
+    private void forbidEating(CallbackInfoReturnable<Boolean> cir) {
+        if (MSVPlayerData.INSTANCE.isInfected(this)) {
+            cir.setReturnValue(false);
+        }
     }
 
     @Unique
@@ -28,34 +37,55 @@ public abstract class InfectedMobsMixin extends MobEntity {
     int CDPlayer = 0;
     @Unique
     int CDAnimal = 0;
+    @Unique
+    private PlayerEntity targetPlayer;
+
     @Inject(method = "mobTick", at = @At("TAIL"))
-    public void looking(CallbackInfo ci) {
+    public void enhancedAI(CallbackInfo ci) {
         if (MSVPlayerData.INSTANCE.isInfected(this)) {
             if (tickCounter > 0) tickCounter--;
             if (CDPlayer > 0) CDPlayer--;
             if (CDAnimal > 0) CDAnimal--;
 
+
             PlayerEntity player = this.getWorld().getClosestPlayer(this.getX(), this.getY(), this.getZ(), 15.0, true);
             if (player != null) {
 
-                if (tickCounter == 400 && Random.Default.nextBoolean()) {
-                    if (MSVPlayerData.INSTANCE.getStage(this) == 0) {
-                        this.lookAtEntity(player, 180, 180);
+                if (tickCounter == 400) {
+                    if (MSVPlayerData.INSTANCE.getStage(this) == 0 && Random.Default.nextBoolean()) {
+                        this.lookControl.lookAt(player, 180, 180);
                     }
                 }
 
-                if (this.distanceTo(player) <= 2.0F && CDPlayer == 0) {
-                    this.lookAtEntity(player, 180, 180);
+                if (this.distanceTo(player) <= 1.5F && CDPlayer == 0) {
+                    this.lookControl.lookAt(player, 180, 180);
                     if (Random.Default.nextBoolean()) {
                         player.damage(new DamageSource(this.getWorld().getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(DamageTypes.MOB_ATTACK), this), 2.0F);
-                        CDPlayer = Random.Default.nextInt(500, 800);
                     }
+                    CDPlayer = Random.Default.nextInt(500, 800);
                 }
 
-                if (tickCounter == 0 && Random.Default.nextBoolean()) {
-                    this.getNavigation().startMovingTo(player.getX(), player.getY(), player.getZ(), 1, 1.4);
-                    this.lookAtEntity(player, 180, 180);
+                if (this.distanceTo(player) < 10.0 && tickCounter == 0) {
+                    if (Random.Default.nextBoolean()) {
+                        this.targetPlayer = player;
+                    }
                     tickCounter = Random.Default.nextInt(700, 900);
+                }
+
+                if (this.targetPlayer != null && this.targetPlayer.isAlive()) {
+                    this.getLookControl().lookAt(this.targetPlayer, 30.0F, 30.0F);
+
+                    if (this.distanceTo(this.targetPlayer) < 10){
+                        this.getNavigation().startMovingTo(this.targetPlayer, 1.4);
+                    }
+                    if (this.distanceTo(this.targetPlayer) < 1.5 || this.distanceTo(this.targetPlayer) > 10) {
+                        this.targetPlayer = null;
+                        this.navigation.stop();
+                    }
+                    if (this.getAttacker() != null) {
+                        this.targetPlayer = null;
+                        this.navigation.stop();
+                    }
                 }
             }
 
